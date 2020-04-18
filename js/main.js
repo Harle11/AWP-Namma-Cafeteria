438964a0e4180a516c019ff52e840dbcccfcf857
Harle11 <shreyasharle919@gmail.com>
2020-04-18 15:50:10 +0530

    ---

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "20%";
+  document.getElementById("main").style.marginLeft = "20%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+function toMenu() {
+  window.location.href = "menu.html";
+}
+
+function toPayments() {
+  window.location.href = "payments.html";
+}
