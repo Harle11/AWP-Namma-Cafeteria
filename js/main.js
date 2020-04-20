9ecde734d4ad8298222aab73c51c7c0fc7d470ff
Rachna2810 <rachnamkp@gmail.com>
2020-04-20 20:28:43 +0530

    Contact page and connectivity wuth home page

--- js/main.js
+++ js/main.js
@@ -15,3 +15,26 @@ function toMenu() {
 function toPayments() {
   window.location.href = "payments.html";
 }
+
+/* --Contact form-- */
+JotForm.init(function(){
+if (window.JotForm && JotForm.accessible) $('input_4').setAttribute('tabindex',0);
+      setTimeout(function() {
+          $('input_6').hint('ex: myname@example.com');
+       }, 20);
+	JotForm.newDefaultTheme = false;
+	JotForm.clearFieldOnHide="disable";
+    /*INIT-END*/
+	});
+
+   JotForm.prepareCalculationsOnTheFly([null,{"name":"clickTo","qid":"1","text":"Feedback Form","type":"control_head"},
+   {"name":"submitForm","qid":"2","text":"Submit Feedback","type":"control_button"},
+   {"name":"feedbackType","qid":"3","text":"Feedback Type","type":"control_radio"},
+   {"name":"describeFeedback","qid":"4","text":"Describe Feedback:","type":"control_textarea"},null,
+   {"name":"email6","qid":"6","text":"E-mail","type":"control_email"},null,{"name":"name","qid":"8","text":"Name","type":"control_fullname"}]);
+   setTimeout(function() {
+JotForm.paymentExtrasOnTheFly([null,{"name":"clickTo","qid":"1","text":"Feedback Form","type":"control_head"},
+{"name":"submitForm","qid":"2","text":"Submit Feedback","type":"control_button"},
+{"name":"feedbackType","qid":"3","text":"Feedback Type","type":"control_radio"},
+{"name":"describeFeedback","qid":"4","text":"Describe Feedback:","type":"control_textarea"},null,
+{"name":"email6","qid":"6","text":"E-mail","type":"control_email"},null,{"name":"name","qid":"8","text":"Name","type":"control_fullname"}]);}, 20);
