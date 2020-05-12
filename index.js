4d61de96ea4a23b3098a3aae416e369be731d6f9
Harle11 <shreyasharle919@gmail.com>
2020-05-12 23:06:56 +0530

    removed unnecessary files

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost/nammaDB', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.error('Connected to Mongoose'))
